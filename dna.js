92caf3008ff75621e25900f808d9e3a1f2d615e6
eliyasson <65622182+eliyasson@users.noreply.github.com>
2022-06-01 00:03:22 +0300

    Update dna.js

--- dna.js
+++ dna.js
@@ -1,3 +1,15 @@
+/* (The science of this might not be accurate)
+Write a function that takes DNA strand as a parameter. 
+The four nucleotides found in DNA are 
+adenine (A), cytosine (C), guanine (G) and thymine (T).
+So, the function should accept any sequence of A, C, G, & T, in any order. 
+A matches with T, G matches with C. 
+
+Your function should return the complement of the given strand. 
+
+e.g. if given G, A, T, C the output should be C, T, A, G
+*/
+
 function encodeDNA1(strand) {
     let DNA = [];
     for (let i = 0; i < strand.length; i+= 1) {
